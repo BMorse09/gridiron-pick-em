95a0cf8a6110f112ce4d21ae33b28d64c6fccfff
BMorse09 <benmorse09@gmail.com>
2016-10-04 20:40:54 -0400

    config change

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module RailsApiProject
     # Cross-Origin Resource Sharing
     config.middleware.use Rack::Cors do
       allow do
-        origins ENV['CLIENT_ORIGIN'] || 'http://localhost:8080'
+        origins ENV['CLIENT_ORIGIN'] || 'https://bmorse09.github.io/gridiron-pick-em-client'
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
